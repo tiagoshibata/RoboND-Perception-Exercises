6ec0010499fe282d92024850b1d5103c1308098e
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-10-14 16:26:47 -0700

    Add histogram and feature calculation

--- Exercise-3/sensor_stick/src/sensor_stick/features.py
+++ Exercise-3/sensor_stick/src/sensor_stick/features.py
@@ -1,13 +1,29 @@
 import matplotlib.colors
-import matplotlib.pyplot as plt
 import numpy as np
-from pcl_helper import *
+from pcl_helper import float_to_rgb
+import sensor_msgs.point_cloud2 as pc2
 
 
 def rgb_to_hsv(rgb_list):
-    rgb_normalized = [1.0*rgb_list[0]/255, 1.0*rgb_list[1]/255, 1.0*rgb_list[2]/255]
+    rgb_normalized = [rgb_list[0] / 255, rgb_list[1] / 255, rgb_list[2] / 255]
     hsv_normalized = matplotlib.colors.rgb_to_hsv([[rgb_normalized]])[0][0]
     return hsv_normalized
+
+
+def compute_histogram(point_list, color_range=(0, 256)):
+    channel_histogram = [None] * 3
+    for channel in range(3):
+        channel_histogram[channel] = np.histogram(
+            (pixel[channel] for pixel in point_list),
+            bins=32,
+            range=color_range)
+
+    # Concatenate and normalize the histograms
+    hist_features = np.concatenate(
+        [channel[0] for channel in channel_histogram]).astype(np.float64)
+    normalized_features = hist_features / np.sum(hist_features)
+
+    return normalized_features
 
 
 def compute_color_histograms(cloud, using_hsv=False):
@@ -15,7 +31,6 @@ def compute_color_histograms(cloud, using_hsv=False):
     # Compute histograms for the clusters
     point_colors_list = []
 
-    # Step through each point in the point cloud
     for point in pc2.read_points(cloud, skip_nans=True):
         rgb_list = float_to_rgb(point[3])
         if using_hsv:
@@ -23,24 +38,7 @@ def compute_color_histograms(cloud, using_hsv=False):
         else:
             point_colors_list.append(rgb_list)
 
-    # Populate lists with color values
-    channel_1_vals = []
-    channel_2_vals = []
-    channel_3_vals = []
-
-    for color in point_colors_list:
-        channel_1_vals.append(color[0])
-        channel_2_vals.append(color[1])
-        channel_3_vals.append(color[2])
-    
-    # TODO: Compute histograms
-
-    # TODO: Concatenate and normalize the histograms
-
-    # Generate random features for demo mode.  
-    # Replace normed_features with your feature vector
-    normed_features = np.random.random(96) 
-    return normed_features 
+    return compute_histogram(point_colors_list)
 
 
 def compute_normal_histograms(normal_cloud):
@@ -55,12 +53,4 @@ def compute_normal_histograms(normal_cloud):
         norm_y_vals.append(norm_component[1])
         norm_z_vals.append(norm_component[2])
 
-    # TODO: Compute histograms of normal values (just like with color)
-
-    # TODO: Concatenate and normalize the histograms
-
-    # Generate random features for demo mode.  
-    # Replace normed_features with your feature vector
-    normed_features = np.random.random(96)
-
-    return normed_features
+    return compute_histogram((norm_x_vals, norm_y_vals, norm_z_vals), color_range=(-1, 1))
